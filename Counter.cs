e38e35e8a285a83f028358b00fdda80c5bd2324c
dheim123 <dheim@midland.k12.ia.us>
2026-01-13 17:52:25 +0000

    acc 1

--- Counter.cs
+++ Counter.cs
@@ -2,7 +2,9 @@
 public class Counter
 {
     public static int Visitors = 100;
-
-    // Add your code here
+    int PrefixIncrement = ++Visitors;
+     int PostfixIncrement = ++Visitors;
+     int PrefixDecrement = --Visitors;
+     int PostfixDecrement = Visitors--;        
     
 }
